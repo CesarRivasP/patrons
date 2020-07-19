c650f356219773cec83aff1f6560712d3e4741cb
cesarrivas10 <cesarjose58@gmail.com>
2020-07-19 19:35:30 -0400

    add patron mediador con ejemplo en node

---
+++ mediador-patron-node.js
@@ -0,0 +1,7 @@
+// ejemplo aplicado a nodeJS
+const Emitter = require('events');
+
+const emitter = new Emitter();
+emitter.on('lala', (x) => console.log(x));
+
+emitter.emit('lala', { lala: 'lolo' });
